4f44700bf8c778bc803c1c917134dca0b569d501
Valentin <valentinm7300@gmail.com>
2019-10-12 16:21:18 +0200

    mise en place des dossier MVC

--- partenaire.php
+++ partenaire.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="public/css/style.css" />
         <title> Extranet groupe bancaire - Projet 3 OpenClassRoom Formation web</title>
     </head>
     <body>
