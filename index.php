01bfdd23395261b870c2727ac0cdf784b25e5c4c
Valentin <valentinm7300@gmail.com>
2019-10-15 21:10:45 +0200

    add CSS for login and register page

--- index.php
+++ index.php
@@ -3,14 +3,20 @@
     <head>
     <title> Connexion </title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="public/css/login.css">
     </head>
     <body>
-        <form action="connexion_post" method="POST" >
-            <label for="email"> Adresse email : </label> <input type="text" name="email" id="email" placeholder="Email" autofocus required /> <br>
-            <label for="password"> Mot de Passe : </label> <input type="password" name="password" id="password" placeholder="Mot de Passe" autofocus required/> <br> 
-            <input type="submit" value="Se Connecter" /> 
-        </form>
-        <a href="register.php"> Se crée un compte</a>
+    <div class="conteneur">
+    <div class="BlocOne">
+            <form  action="connexion_post" method="POST" >
+                <label for="email"> Adresse email : </label> <input type="text" name="email" id="email" placeholder="Email" autofocus required /> <br>
+                <label for="password"> Mot de Passe : </label> <input type="password" name="password" id="password" placeholder="Mot de Passe" autofocus required/> <br> 
+                <input type="submit" value="Se Connecter" /> 
+            </form>
+        <div class="registerLink">
+            <a href="register.php"> Se crée un compte</a>
+        </div>
+    </div>
+    </div>
     </body>
 </html>
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="public/css/register.css">
         <title> Inscription </title>
     </head>
     <body>
