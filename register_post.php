682bdcf66cfe9ddfacf969cf61a7e4b014790adc
Valentin <valentinm7300@gmail.com>
2019-10-22 22:51:16 +0200

    réorganisation du code register_post
    22/10/2019

--- register_post.php
+++ register_post.php
@@ -1,18 +1,25 @@
 <?php
-    try {
-        $bdd = new PDO ('mysql:host=localhost;dbname=gbaf_member;charset=utf8', 'root' ,'');
-        
-    } catch (Exception $e) {
-        die ('Erreur :' .$e->getMessage());
-    }
-    if ($_POST ['password'] != $_POST['repeat_password']) {
+if  (isset($_POST['firstName']) AND isset($_POST['lastName']) AND isset($_POST['email']) AND isset($_POST['password']) AND isset($_POST['repeat_password']))
+{
+    if ($_POST ['password'] != $_POST['repeat_password']) 
+    {
         echo 'Wrong password';
-      echo   ' <p> <a href="register.php"> recommencer </a> </p>';
+        echo   ' <p> <a href="register.php"> recommencer </a> </p>';
     }
-    else {
-        $req = $bdd->prepare('INSERT INTO member (firstName, lastName, email, password, repeat_password, created_at) 
+        else 
+        {
+            $_POST['password'] = md5($_POST['password']);
+            try 
+            {
+                $bdd = new PDO ('mysql:host=localhost;dbname=gbaf_member;charset=utf8', 'root' ,'');
+            }   
+                catch (Exception $e) 
+                {
+                    die ('Erreur :' .$e->getMessage());
+                }
+            $req = $bdd->prepare('INSERT INTO member (firstName, lastName, email, password, repeat_password, created_at) 
                         VALUES (:firstName, :lastName, :email, :password, :repeat_password, NOW() )');
-                $req->execute(array(
+                         $req->execute(array(
                         'firstName' => htmlentities($_POST['firstName']), 
                         'lastName' => htmlentities($_POST['lastName']), 
                         'email' => htmlentities($_POST['email']), 
@@ -21,4 +28,4 @@
                         ));
     header('Location: index.php');
     }
-        
+}
