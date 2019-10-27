06e3cf412e18169ace898a8e44dca5434b2d1609
Valentin <valentinm7300@gmail.com>
2019-10-27 16:42:23 +0100

    FIN du code  page isncription

--- model/insert_bd.php
+++ model/insert_bd.php
@@ -1,15 +1,23 @@
 <?php
-function insert()  
+function insert($firstName, $lastName, $email, $password)  
 {   
     require ('connect_bd.php');
+    try 
+    {
+        
     $bdd = connect();
     $req = $bdd->prepare('INSERT INTO member (firstName, lastName, email, password, created_at) 
     VALUES (:firstName, :lastName, :email, :password, NOW() )');
         $req->execute(array(
-        'firstName' => htmlentities($_POST['firstName']), 
-        'lastName' => htmlentities($_POST['lastName']), 
-        'email' => htmlentities($_POST['email']), 
-        'password' => htmlentities($_POST['password']),
+        'firstName' => $firstName , 
+        'lastName' => $lastName , 
+        'email' => $email , 
+        'password' => $password ,
         ));
     return $req;
+    } 
+    catch (\Throwable $th) 
+        {
+        return NULL;
+        }
 }
--- register_post.php
+++ register_post.php
@@ -1,33 +1,27 @@
 <?php
         require ('model/insert_bd.php');
-if (isset($_POST['submit'])) 
-{
-    if (!empty($_POST['firstName']) AND !empty($_POST['lastName']) AND !empty($_POST['email']) AND !empty($_POST['password']))
-    {
-        if (strlen($_POST['password']) >= 6 ) 
-        {
-            if ($_POST ['password'] == $_POST['repeat_password'])
-            {
-                $_POST['password'] = md5($_POST['password']);
-                $req = insert();
-                header('Location: index.php');
+if (isset($_POST['submit']) AND !empty($_POST['firstName']) AND !empty($_POST['lastName']) AND !empty($_POST['email']) AND !empty($_POST['password']) 
+    AND (strlen($_POST['password']) >= 6 ) AND ($_POST ['password'] == $_POST['repeat_password']))
+            
+            { 
+                $firstName = htmlentities($_POST['firstName']);
+                $lastName = htmlentities($_POST['lastName']);
+                $email = htmlentities($_POST['email']);
+                $password = md5(htmlentities ($_POST['password']));
+                $req = insert($firstName, $lastName, $email, $password);
+                if ($req !== NULL) 
+                {
+                    header('Location: index.php');
+                }
+                else 
+                {
+                    echo 'Erreur dans base de donnée';
+                }
+                
             }
                 else
                 {
-                    echo 'Les mots de passes ne sont pas identiques !';
+                    echo 'Les mots de passes ne sont pas identiques !'; //changer la phrase
                     echo   ' <p> <a href="view/register_view.php"> recommencer </a> </p>';
                 }
-        }
-            else 
-            {
-                echo 'Votre mots de passe est trop petit';
-                echo   ' <p> <a href="view/register_view.php"> recommencer </a> </p>';
-            }
-    }
-        else 
-        {
-            echo 'La totaliter des champs ne sont pas remplis';
-            echo   ' <p> <a href="view/register_view.php"> recommencer </a> </p>';
-        }
-}
 // NOTE : Rajouter les vérifications si : déjà un compte / email déjà use / contenu des champs.  s'orienter vers les regex ou boucle while
