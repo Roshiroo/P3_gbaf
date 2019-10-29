f6df3f12d0351da42a1413afa7e8fa4642926411
Valentin <valentinm7300@gmail.com>
2019-10-29 21:13:58 +0100

    Update connexion_post.php

--- connexion_post.php
+++ connexion_post.php
@@ -5,13 +5,13 @@
     {
         
         $email = htmlspecialchars($_POST['email']);
-        $password = md5(htmlspecialchars ($_POST['password']));
+        $password = htmlspecialchars ($_POST['password']);
         $bdd = connect();
-        $req = $bdd->prepare('SELECT * FROM member  WHERE email = ? , password = ?');
+        $req = $bdd->prepare('SELECT * FROM member  WHERE email = ? AND password = ?');
         $req->execute(array( $email, md5($password)));
         if ($req -> rowCount() == 1 ) 
         {
-            echo 'fount !';
+            header('Location: view/partenaire_view.php');
         }
         else 
         {
