f134641ed12ad2acb66e11c02cb27f191bc508e3
Valentin <valentinm7300@gmail.com>
2019-11-01 14:51:36 +0100

    optimisation du code
    01/11/2019

--- connexion_post.php
+++ connexion_post.php
@@ -1,17 +1,15 @@
 <?php
 session_start(); 
-    require ('model/connect_bd.php');
-
+    require ('verifyConnect.php');
     if (isset($_POST['submit']) AND !empty($_POST['email']) AND !empty($_POST['password']))
     {
         
         $email = htmlspecialchars($_POST['email']);
         $password = htmlspecialchars ($_POST['password']);
-        $bdd = connect();
-        $req = $bdd->prepare('SELECT * FROM member  WHERE email = ? AND password = ?');
-        $req->execute(array( $email, md5($password)));
-        if ($req -> rowCount() == 1 ) 
+        $req = verifyConnect($email, $password);
+        if ($req -> rowCount() == 1 ) //si mdp en double dans la bdd , ne connecte pas
         {
+            
             $user = $req->fetch();
             $_SESSION['user'] = $user;
             header('Location: view/partenaire_view.php');
---
+++ verifyConnect.php
@@ -0,0 +1,16 @@
+<?php
+function verifyConnect($email, $password) 
+{
+    require ('model/connect_bd.php');
+    try 
+    {
+    $bdd = connect();
+    $req = $bdd->prepare('SELECT * FROM member  WHERE email = ? AND password = ?');
+    $req->execute(array( $email, md5($password)));
+    return $req;
+    } 
+        catch (\Throwable $th) 
+        {
+        return NULL;
+        }
+}   
